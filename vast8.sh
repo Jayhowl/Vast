6c013333632f066fbb10bba7a400284e693662da
Jayhowl <101426739+Jayhowl@users.noreply.github.com>
2023-10-22 06:44:54 +0100

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -27,8 +27,8 @@ sudo apt -y install python3-pip
 sleep 5
 echo "---------------------------"
 
-# Clone the repository and build the project https://github.com/shanhaicoder/XENGPUMiner.git
-git clone https://github.com/Jayhowl/XENGPUMiner.git
+# Clone the repository and build the project https://github.com/Jayhowl/XENGPUMiner.git
+git clone https://github.com/shanhaicoder/XENGPUMiner.git
 sleep 5
 echo "---------------------------"
 
