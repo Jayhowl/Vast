f0968bcb99cacfad3b14e1690b41f88b1fad0bfe
obi-wan-xenobi <101426739+obi-wan-xenobi@users.noreply.github.com>
2023-11-10 21:49:41 +0000

    Update vast14_obi.sh

--- vast14_obi.sh
+++ vast14_obi.sh
@@ -105,6 +105,7 @@ echo "---------------------------"
 sudo nohup ./xengpuminer -d11 > xengpuminer-11.log 2>&1 &
 sleep 5
 echo "---------------------------"
+
 sudo nohup ./xengpuminer -d12 > xengpuminer-12.log 2>&1 &
 sleep 5
 echo "---------------------------"
